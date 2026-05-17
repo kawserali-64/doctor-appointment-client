4d5020536bccadad8d80871e8f26d7798428b12e
kawserali-64 <kawserali6436@gmail.com>
2026-05-17 23:07:22 +0600

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+
+const NavBarPage = () => {
+    return (
+        <div>
+
+       <h1 className="text-3xl font-bold">
+        navbar
+      </h1>     
+        </div>
+    );
+};
+
+export default NavBarPage;
